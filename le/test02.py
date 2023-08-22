cb595fcccd82e7c742aef11d0cf0b3e189419f41
6652410005ChanachonPimpun <s6652410005@sau.ac.th>
2023-08-22 10:44:53 +0700

    code V.1: Create 4 file lol

---
+++ le/test02.py
@@ -0,0 +1,3 @@
+print('welcome to "SAU"')
+print('welcome to "DTI"')
+print(134658)
---
+++ le/test03.py
@@ -0,0 +1,4 @@
+print('SAU',666,666.444,True,10+50)
+print('SAU'+str(666)+''+str(666.444)+''+str(True)+''+str(10+15))
+print('SAU ()()()()'.format(666,666.444,True,10+50))
+print
